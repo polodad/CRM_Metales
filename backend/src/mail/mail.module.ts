dd3a7c104fa0aedc89222a14b72ac74b8687cbe2
Tu Nombre <tu-email@ejemplo.com>
2026-01-17 18:18:06 -0600

    Fix SMTP secure boolean logic

--- backend/src/mail/mail.module.ts
+++ backend/src/mail/mail.module.ts
@@ -11,7 +11,7 @@ import { MailService } from './mail.service';
                 transport: {
                     host: config.get('SMTP_HOST'),
                     port: config.get('SMTP_PORT', 587),
-                    secure: config.get('SMTP_SECURE', false),
+                    secure: config.get('SMTP_SECURE') === 'true',
                     auth: {
                         user: config.get('SMTP_USER'),
                         pass: config.get('SMTP_PASS'),
